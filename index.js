a8f8e5a4da7f2063bd57c76466e73f3f3f6fc7f1
Aditya-619 <ranjanaditya5678@gmail.com>
2024-01-17 16:22:56 +0530

    start express

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-import http from 'http'
-import { readFileSync } from 'fs'
 
-const index = readFileSync('data.json', 'utf-8')
-const data = readFileSync('info.txt', 'utf-8');
+import fs from 'fs'
+import express from 'express'
 
+const index = fs.readFileSync('data.json', 'utf-8')
 
-const server = http.createServer((req, res)=>{
-    console.log(req.url)  // when working with HTTP requests, the req.url property is part of the request object and represents the URL of the incoming request. It includes the path, query parameters, and any other relevant information.
-    console.log("server started")
-    // res.setHeader('Content-Type', 'text/html')
-    // res.end("<h1>Aditya Hero</h2>")
+const server = express()
 
-    // res.setHeader('Content-Type', 'application/json')
-    // res.end(index)
-
-    // Routing
-    switch(req.url) {
-        case '/': 
-            res.setHeader('Content-Type', 'application/json')
-            res.end(index);
-            break;
-        case '/info':
-            res.setHeader('Content-Type', 'text/html');
-            res.end(data);
-            break;
-        default:
-            res.writeHead(404);
-            res.end();
-    }
+server.get('/', (req, res)=>{
+    res.send('hello!')
 })
 
-server.listen(8080)
+server.listen(8080, '0.0.0.0') 
